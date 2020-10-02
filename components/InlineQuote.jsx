f080cdf633eb1e9a248819b1144dac4c28f28bb3
MulverineX <mulverin3@gmail.com>
2020-10-02 17:13:23 -0600

    Update InlineQuote.jsx

--- components/InlineQuote.jsx
+++ components/InlineQuote.jsx
@@ -10,7 +10,7 @@ module.exports = class InlineQuote extends React.Component {
       <>
         <div key={this.props.content} className='qo-inline'>
           <div className='qo-header header-23xsNx threads-header-hack'>
-            <img src={this.props.author.avatarURL} aria-hidden="true" class={`qo-avatar threads-avatar-hack avatar-1BDn8e clickable-1bVtEA`} alt=" "></img>
+            <img src={this.props.author.avatarURL} aria-hidden="true" class={`qo-avatar threads-avatar-hack revert-reply-hack avatar-1BDn8e clickable-1bVtEA`} alt=" "></img>
             <div className='qo-username username-1A8OIy clickable-1bVtEA'>{this.props.author.username}</div>
           </div>
           <div className='qo-content'>
