4bc83536dcea100189261bf49ba2004853fe698a
MulverineX <mulverin3@gmail.com>
2020-10-03 01:18:22 -0600

    Clean up code

--- components/InlineQuoteContainer.jsx
+++ components/InlineQuoteContainer.jsx
@@ -14,6 +14,7 @@ const parser = getModule(["parse", "parseTopic"], false).parse;
 const quote = require('./InlineQuote')
 //console.log(blockquoteContainer)
 let lastFetch;
+
 module.exports = class InlineQuoteContainer extends React.Component {
   constructor (props) {
     super(props);
@@ -26,15 +27,15 @@ module.exports = class InlineQuoteContainer extends React.Component {
      return state
   }
   async componentDidMount () {
-    const content = [...this.state.content]
+    const content = [...this.state.content];
     //console.log(content)
-    content.forEach(async (e, i) => {
-      if (e && e.props && e.props.href && (/https?:\/\/((canary|ptb)\.)?discord(app)?\.com\/channels\/(\d{17,19}|@me)\/\d{17,19}\/\d{17,19}/g).test(e.props.href)) {
+    content.forEach(async (e, i) => { if (e && e.props) {
+      if (e.props.href && (/https?:\/\/((canary|ptb)\.)?discord(app)?\.com\/channels\/(\d{17,19}|@me)\/\d{17,19}\/\d{17,19}/g).test(e.props.href)) {
         const linkArray = e.props.href.split('/');
         const messageData = await this.getMsgWithQueue(linkArray[5], linkArray[6]);
-        if (!messageData) {
-          return;
-        }
+
+        if (!messageData) return;
+
         //console.log(messageData)
         if (messageData.embeds) {
           messageData.embeds.forEach((e, i) => {
@@ -44,27 +45,28 @@ module.exports = class InlineQuoteContainer extends React.Component {
             }
           });
         }
+
         //msg.message.content = msg.message.content.replace(e.props.href, '')
         content[i] = React.createElement(quote, {
           className: `${message} ${cozyMessage} ${groupStart}`,
           groupId: messageData.id,
-          message: new MessageC({
-            ...messageData,
-          }),
+          message: new MessageC({ ...messageData }),
           channel: getChannel(messageData.channel_id),
           author: messageData.author,
           content: parser(messageData.content, true, { channelId: this.props.message.channel_id }),
-          onClick: () => { transitionTo(e.props.href.replace(/https?:\/\/((canary|ptb)\.)?discord(app)?\.com/g, '')); },
+          onClick: () => transitionTo(e.props.href.replace(/https?:\/\/((canary|ptb)\.)?discord(app)?\.com/g, '')),
           style: { cursor: "pointer" }
         });
       }
-      if (e && e.props && e.props.className && e.props.className === blockquoteContainer && (content[i + 1] && content[i + 1].props && content[i + 1].props.children && content[i + 1].props.children.props && content[i + 1].props.children.props.className && content[i + 1].props.children.props.className.includes('mention'))) {
+
+      if (e.props?.className === blockquoteContainer && content[i + 1]?.props?.children?.props?.className.includes('mention')) {
         //msg.message.content = msg.message.content.replace(e.props.href, '')
-        const messageData = /(?:> )([\s\S]+)(<@!?(\d+)>)/g.exec(this.props.message.content)
-        console.log(messageData, messageData[1].replace(/\n> /g, ''))
-        content[i+1] = null
+        const messageData = /(?:> )([\s\S]+)(<@!?(\d+)>)/g.exec(this.props.message.content);
+        //console.log(messageData, messageData[1].replace(/\n> /g, ''))
+        content[i + 1] = null;
+        
         content[i] = React.createElement(quote, {
-          //className: `${message} ${cozyMessage} ${groupStart}`,
+          className: `${message} ${cozyMessage} ${groupStart}`,
           author: getUser(messageData[3]),
           content: parser(messageData[1].replace(/\n> /g, '\n').replace(/\n$/g, '').trim(), true, { channelId: this.props.message.channel_id }),
           channel: getChannel(this.props.message.channel_id)
@@ -72,34 +74,34 @@ module.exports = class InlineQuoteContainer extends React.Component {
           //style: { cursor: "pointer" }
         });
       }
-    });
+    }});
     this.setState({ content, new: true });
     setTimeout(() => {
       this.forceUpdate()
     }, 500);
   }
 
-      // queue based on https://stackoverflow.com/questions/53540348/js-async-await-tasks-queue
-      getMsgWithQueue = (() => {
-        let pending = Promise.resolve()
+    // queue based on https://stackoverflow.com/questions/53540348/js-async-await-tasks-queue
+    getMsgWithQueue = (() => {
+      let pending = Promise.resolve()
 
-        const run = async (channelId, messageId) => {
-            try {
-                await pending
-            } finally {
-                return this.getMsg(channelId, messageId)
-            }
-        }
+      const run = async (channelId, messageId) => {
+          try {
+              await pending
+          } finally {
+              return this.getMsg(channelId, messageId)
+          }
+      }
 
-        return (channelId, messageId) => (pending = run(channelId, messageId))
+      return (channelId, messageId) => (pending = run(channelId, messageId))
     })()
 
   async getMsg (channelId, messageId) {
     let message = getMessage(channelId, messageId);
+
     if (!message) {
-      if (lastFetch > Date.now() - 2500) {
-        await new Promise(r => setTimeout(r, 2500));
-      }
+      if (lastFetch > Date.now() - 2500) await new Promise(r => setTimeout(r, 2500));
+      
       const data = await get({
         url: Endpoints.MESSAGES(channelId),
         query: {
@@ -110,9 +112,9 @@ module.exports = class InlineQuoteContainer extends React.Component {
       });
       lastFetch = Date.now();
       message = data.body.find(m => m.id == messageId);
-      if (!message) {
-        return;
-      }
+
+      if (!message) return;
+
       message.author = new User(message.author);
       message.timestamp = new Timestamp(message.timestamp);
     }
@@ -120,8 +122,6 @@ module.exports = class InlineQuoteContainer extends React.Component {
   }
 
   render () {
-    return (
-        <div key={this.state.content}>{this.state.content}</div>
-    )
-    }
+    return ( <div key={this.state.content}>{this.state.content}</div> )
+  }
 };
