5da4ef6121912693b2311a39efb910c5c0bbe9b4
AAGaming00 <aa@bigdumb.gq>
2020-11-27 23:58:54 -0500

    almost works tm

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Plugin } = require('powercord/entities');
 const { inject, uninject } = require('powercord/injector');
 const { getModule, React, ReactDOM } = require('powercord/webpack');
-
+const { getReactInstance } = require('powercord/util');
 const Renderer = require('./components/Renderer');
 
 const Quote = require('./components/Quote');
@@ -15,16 +15,18 @@ const parseRaw = require('./utils/parseRaw.js');
 const settings = require('./utils/settings.js');
 
 module.exports = class RichQuotes extends Plugin {
-  getSettings() {
-    let resolved = {};
+  getSettings () {
+    const resolved = {};
 
-    Object.entries(settings.list).forEach(([ key, setting ]) => { resolved[key] = this.settings.get(key, setting.fallback) })
+    Object.entries(settings.list).forEach(([ key, setting ]) => {
+      resolved[key] = this.settings.get(key, setting.fallback);
+    });
 
     return resolved;
-  };
+  }
 
   async startPlugin () {
-    powercord.api.settings.registerSettings("rich-quotes", {
+    powercord.api.settings.registerSettings('rich-quotes', {
       label: 'Rich Quotes',
       category: this.entityID,
       render: Settings
@@ -34,7 +36,9 @@ module.exports = class RichQuotes extends Plugin {
 
     const cacheSearch = this.settings.get('cacheSearch', true);
 
-    if (!cacheSearch && window.localStorage.richQuoteCache) window.localStorage.removeItem('richQuoteCache');
+    if (!cacheSearch && window.localStorage.richQuoteCache) {
+      window.localStorage.removeItem('richQuoteCache');
+    }
 
     const Style = await getModule([ 'mentioned' ]);
 
@@ -51,60 +55,76 @@ module.exports = class RichQuotes extends Plugin {
     Object.assign(ListMessage.type, lmType);
   }
 
-  injectMessage(args, res, Style, list = false) {
+  injectMessage (args, res, Style, list = false) {
     if (res) {
-      let resContent = res.props.childrenMessageContent;
+      const resContent = res.props.childrenMessageContent;
 
       let parsed;
 
       const settings = this.getSettings();
 
       if (resContent) {
-        parsed = parseRaw((' ' + args.message.content).slice(1).split('\n'));
+        parsed = parseRaw((` ${args.message.content}`).slice(1).split('\n'));
 
         if (!parsed.isCommand) {
           if (parsed.quotes || parsed.hasLink) {
-            if (parsed.quotes && !parsed.broadMention)
-            res.props.className = res.props.className.replace(Style.mentioned, '');
+            if (parsed.quotes && !parsed.broadMention) {
+              res.props.className = res.props.className.replace(Style.mentioned, '');
+            }
 
-            resContent.props.content = React.createElement(Renderer, { 
-              content: resContent.props.content, message: args.message, 
-              quotes: parsed.quotes, broadMention: (list ? !list : parsed.broadMention),
-              level: 0, settings
+            resContent.props.content = React.createElement(Renderer, {
+              content: resContent.props.content,
+              message: args.message,
+              quotes: parsed.quotes,
+              broadMention: (list ? !list : parsed.broadMention),
+              level: 0,
+              settings
             });
 
-            if (args.message.author.bot && settings.cullBotQuotes) args.message.embeds = [];
+            if (args.message.author.bot && settings.cullBotQuotes) {
+              args.message.embeds = [];
+            }
           }
-        } else if (!list && settings.cullQuoteCommands) res = null;
+        } else if (!list && settings.cullQuoteCommands) {
+          res = null;
+        }
       }
 
       if (res && settings.replyReplace && !res.props.className.includes('rq-message-reply')) {
-        let resReply = res.props.childrenRepliedMessage;
+        const resReply = res.props.childrenRepliedMessage;
 
         let reply = resReply?.props?.children?.props?.referencedMessage?.message;
 
-        if (!reply) reply = resReply?.props?.referencedMessage?.message;
+        if (!reply) {
+          reply = resReply?.props?.referencedMessage?.message;
+        }
 
         if (reply) {
-          res.props.childrenRepliedMessage = null;
+          res.props.childrenRepliedMessage = React.createElement('div', {
+            ref: (e) => {
+              if (!e) {
+                return;
+              }
+              console.dir(e);
+              const target = getReactInstance(e).sibling.child.child.child.sibling.stateNode;
 
-          if (settings.replyMode == 0) {
-            // :keuch: couldn't attach ref to parent or child
-            let target = document.getElementById(`chat-messages-${args.message.id}`).firstElementChild.childNodes[1].firstElementChild.childNodes[4];
+              if (settings.replyMode == 0) {
+                const avatarImage = React.createElement(Avatar, {
+                  user: res.props.childrenHeader.props.referencedMessage.message.author
+                });
 
-            const avatarImage = React.createElement(Avatar, {
-              user: res.props.childrenHeader.props.referencedMessage.message.author 
-            });
 
-            let container = document.createElement('div');
+                const container = document.createElement('div');
 
-            container.className = 'rq-avatar-wrapper';
+                container.className = 'rq-avatar-wrapper';
 
-            if (target.childNodes.length === 1) {
-              ReactDOM.render(avatarImage, container);
-              target.prepend(container);
+                if (target.childNodes.length === 1) {
+                  ReactDOM.render(avatarImage, container);
+                  target.prepend(container);
+                }
+              } else res.props.className = `${res.props.className} rq-hide-reply-header`;
             }
-          } else res.props.className = `${res.props.className} rq-hide-reply-header`;
+          });
 
           const location = args.message.messageReference;
 
@@ -116,17 +136,25 @@ module.exports = class RichQuotes extends Plugin {
             if (parsed && !parsed.broadMention) {
               mentionType = 2;
               res.props.className = res.props.className.replace(Style.mentioned, '');
-            } else mentionType = 3;
+            } else {
+              mentionType = 3;
+            }
           }
 
           const renderedQuote = React.createElement(Quote, {
             link: [ location.guild_id, location.channel_id, location.message_id ],
             parent: [ parentLocation[4], parentLocation[5], args.message.id ],
-            mentionType, level: 0, isReply: true, settings
+            mentionType,
+            level: 0,
+            isReply: true,
+            settings
           });
 
-          if (Array.isArray(resContent.props.content)) resContent.props.content.unshift(renderedQuote);
-          else resContent.props.content.props.content.unshift(renderedQuote);
+          if (Array.isArray(resContent.props.content)) {
+            resContent.props.content.unshift(renderedQuote);
+          } else {
+            resContent.props.content.props.content.unshift(renderedQuote);
+          }
 
           res.props.rq_setReply = true;
         }
@@ -137,7 +165,7 @@ module.exports = class RichQuotes extends Plugin {
   }
 
   pluginWillUnload () {
-    powercord.api.settings.unregisterSettings("rich-quotes");
+    powercord.api.settings.unregisterSettings('rich-quotes');
     uninject('Rich-Quotes-Channel-Message');
     uninject('Rich-Quotes-List-Message');
   }
