d1e3b1f022a6065f31b1b6d88b44475e60fe12a7
Mulverine <mulverin3@gmail.com>
2021-05-27 23:47:17 -0600

    Fix avatars

--- components/child/Avatar.jsx
+++ components/child/Avatar.jsx
@@ -1,5 +1,8 @@
 const { React } = require('powercord/webpack');
 
 module.exports = function ({ style, user }) {
-  return (<img className={`rq-avatar threads-avatar-hack revert-reply-hack ${style.avatar} ${style.clickable}`} src={user.avatarURL} aria-hidden="true" alt=" " />);
+  return (<img 
+    className={`rq-avatar threads-avatar-hack revert-reply-hack ${style.avatar} ${style.clickable}`} 
+    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.png?size=32`} 
+    aria-hidden="true" alt=" " />);
 };
